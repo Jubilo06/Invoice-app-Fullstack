706982c9563d202728d7e3d3417ec67126776448
jubilo <amusantemiloluwa78@gmail.com>
2025-09-24 14:01:07 +0100

    Seventeenth fix(build): Use npx to run rimraf for better compatibility

--- backend/routes/index.mjs
+++ backend/routes/index.mjs
@@ -3,7 +3,7 @@ import usersRouter from "./users.mjs";
 import invoicesRouter from "./invoices.mjs";
 
 const router = Router();
-router.use(usersRouter);
+router.use('/api/users',usersRouter);
 router.use(invoicesRouter);
 
 export default router;
